6dc6f699e569f7e4ebd18be7765c4a2f0dcba76a
deezheng@163.com <214114@qq.com>
2025-07-13 00:49:58 +0800

    Initial commit: Setup VitePress for diary

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,28 @@
+
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "我的日记",
+  description: "一个 VitePress 站点",
+  base: '/daily/',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+    ],
+
+    sidebar: [
+      {
+        text: '日记',
+        items: [
+          { text: '第一篇', link: '/posts/1' },
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/maomao1996/daily' }
+    ]
+  }
+})
